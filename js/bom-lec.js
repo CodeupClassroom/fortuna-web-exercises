e0bfdf959c5ccc1208c40087055efb5d1a2a7766
David Stephens <stephens.a.david@gmail.com>
2020-02-18 14:08:17 -0600

    completed the bom walkthrough

--- js/bom-lec.js
+++ js/bom-lec.js
@@ -2,15 +2,34 @@
 
 (function() {
 
+    // var annoyingMessage = setTimeout(function() {
+    //     alert("If you were a hot dog, and you were starving; would you eat yourself?");
+    // }, 5000);
 
+    // document.getElementsByTagName("button")[0].addEventListener("click", function() { // We'll talk about this line of code tomorrow.
+    //     clearTimeout(annoyingMessage);
+    // });
 
     document.getElementsByTagName("button")[0].addEventListener("click", function() { // We'll talk about this line of code tomorrow.
-        window.location = "https://developer.mozilla.org/en-US/docs/Web/API/Window/location";
+        window.location = "https://codeup.com";
     });
 
-    setInterval(function() {
-        alert("You've been looking at your tax information for too long...");
-        location.reload(true);
-    }, 60000);
+    // setTimeout(function() {
+    //     alert("You've been looking at your tax information for too long...");
+    // }, 5000);
+    //
+    // var stillActiveCheck = setInterval(function() {
+    //     var stillThere = confirm("You still there?");
+    //     if (stillThere) {
+    //         clearInterval(stillActiveCheck);
+    //     }
+    // }, 5000);
+    //
+    // var stillActiveCheck2 = setInterval(function() {
+    //     var stillThere = confirm("You still there, my dude?");
+    //     if (stillThere) {
+    //         clearInterval(stillActiveCheck2);
+    //     }
+    // }, 7000);
 
 })();
