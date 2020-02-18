2ff62076d2775ddf37bcf54da0e8016d58671a3d
David Stephens <stephens.a.david@gmail.com>
2020-02-18 10:13:48 -0600

    completed the math object lecture

---
+++ js/math-lec.js
@@ -0,0 +1,39 @@
+"use strict";
+
+var max = 30;
+var min = 25;
+// console.log(Math.floor(Math.random() * (max - min + 1)) + min);
+
+// Math.floor = Rounds down.
+
+// console.log(Math.floor(9.99999999999999));
+
+// Math.ceil = Rounds up
+
+// console.log(Math.ceil(3.00000001));
+
+// Math.round = Rounds according to the rules we (probably) learned in grammar school.
+
+// console.log(Math.round(5.5));
+// console.log(Math.round(3.14));
+// console.log(Math.round(-2.5));
+// console.log(Math.round(-33.7));
+
+// Math.sqrt = return square root of a number.
+
+// console.log(Math.sqrt(144));
+console.log(Math.sqrt("bananas"));
+console.log(Math.sqrt(false));
+console.log(Math.sqrt(parseFloat("99 red balloons")));
+
+
+
+// Math.pow = return a number to a power. The first number is the integer, and the second number is the exponent.
+
+console.log(Math.pow(4, 2));
+
+console.log(Math.sqrt(Math.pow(4, 3)));
+
+// Math.PI = 3.14159
+console.log(Math.PI * 4);
+
