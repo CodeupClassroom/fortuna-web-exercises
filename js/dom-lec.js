374c6b0d91d0a81f0615039544e494caa7460bc4
David Stephens <stephens.a.david@gmail.com>
2020-02-19 10:00:19 -0600

    completed the dom lecture

--- js/dom-lec.js
+++ js/dom-lec.js
@@ -2,9 +2,42 @@
 
 (function(){
 
-    // document.getElementById("header").innerText = "Learning is P Dumb :)";
-    // document.getElementsByClassName("grandpa")[0].innerHTML = "<h1>Look ma, I broke it!</h1>";
-    // document.getElementsByTagName("p")[0].innerText = "Look at those pretty dots!!!";
+    var header = document.getElementById("header");
+
+    header.style.backgroundColor = "#ef0";
+
+    header.innerText = "Document Object Model";
+
+    // console.log(header);
+    var grandpa = document.getElementsByClassName("grandpa")[0];
+
+    // console.log(grandpa);
+
+    var paragraph = document.getElementsByTagName("p")[0];
+
+    var pText = paragraph.innerText;
+
+    // console.log(pText);
+
+    // paragraph.innerHTML = pText + "<br><img alt='pixel art' src='img/pixel-art.gif'>";
+
+    // console.log(paragraph);
+
+    var body = document.getElementsByTagName("body")[0];
+
+    // body.innerHTML = "<h1>Victor made me break the Page</h1>";
+
+    // var img = document.getElementsByTagName("img")[0];
+    //
+    // console.log(img.hasAttribute("class"));
+    //
+    // if (!img.hasAttribute("class")) {
+    //     img.setAttribute("class", "fancy-gif billboard nice-scenery");
+    // }
+    //
+    // img.removeAttribute("class");
+
+    console.log(img.hasAttribute("class"));
 
     document.getElementById("login-btn").addEventListener("click", function(e) {
         e.preventDefault();
@@ -12,7 +45,5 @@
         var password = document.forms.login.password.value;
         alert("Username: " + username +"\nPassword: " + password);
     });
-
-
 
 })();
